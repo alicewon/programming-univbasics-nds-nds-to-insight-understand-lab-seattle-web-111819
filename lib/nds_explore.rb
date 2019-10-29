15be5eb0a46c5019ba0999077bad96e1704158c1
Alice Won <alicewon4@gmail.com>
2019-10-29 04:07:59 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -16,4 +16,11 @@ def print_first_directors_movie_titles
   #while row_index < directors_database.length do
   #  puts "Row #{row_index}"
   #end
+
+  row_index = 0
+  while row_index < directors_database[0].length do
+    print directors_database[0][:movies][:title]
+  end
+
+  directors_database[0]
 end
